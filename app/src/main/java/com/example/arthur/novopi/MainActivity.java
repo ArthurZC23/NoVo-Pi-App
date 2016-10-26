37b526856d4c865d6e1d97d573c6c76def6b0988
Arthur Zanatta da Costa <arthurzc23@gmail.com>
2016-10-26 12:01:02 -0200

    Initial commit

---
+++ app/src/main/java/com/example/arthur/novopi/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.arthur.novopi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.MotionEvent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText addressEditText;
+    private TextView addressTextView;
+    private Spinner webClientSpinner;
+    private Button webClientButton;
+    private String novopiIPAdress;
+    private String webClientPath;
+    private Intent webIntent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addressTextView = (TextView) findViewById(R.id.addressTextView);
+        addressEditText = (EditText) findViewById(R.id.addressEditText);
+        webClientSpinner = (Spinner) findViewById(R.id.webClientSpinner);
+        webClientButton = (Button)  findViewById(R.id.webClientButton);
+
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.web_clients, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        webClientSpinner.setAdapter(spinnerAdapter);
+
+        addressEditText.addTextChangedListener(addressEditTextWatcher);
+        webClientButton.setOnClickListener(webClientButtonListener);
+    }
+
+    private TextWatcher addressEditTextWatcher = new TextWatcher(){
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count){}
+
+        @Override
+        public void  afterTextChanged(Editable s) {
+
+            novopiIPAdress = s.toString();
+            addressTextView.setText(novopiIPAdress);
+
+        }
+
+        @Override
+        public void  beforeTextChanged(CharSequence s, int start, int count, int after){}
+    };
+
+    public View.OnClickListener webClientButtonListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v){
+
+            webClientPath = "http://" + novopiIPAdress + ":6680/" + webClientSpinner.getSelectedItem().toString();
+            webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(webClientPath));
+            try {
+                startActivity(webIntent);
+            }catch (Exception e) {}
+
+        }
+
+    };
+}
